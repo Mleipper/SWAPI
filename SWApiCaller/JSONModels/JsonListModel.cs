6de56dd95db8ae9e18240840635bd3c848b7037d
m <mleipper@hotmail.com>
2019-11-25 21:18:45 +0000

    Began to look at Using generics

---
+++ SWApiCaller/JSONModels/JsonListModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWApiCaller.JSONModels
+{
+    public class JsonListModel<T> where T : class
+    {
+        public int count { get; set; }
+
+        public string next { get; set; }
+
+        public object previous { get; set; }
+
+        public List<T> results { get; set; }
+    }
+}
